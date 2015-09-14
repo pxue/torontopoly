52ab3187a488d0389a5f410daafe89e88e8551b3
Paul Xue <paul.xue@pressly.com>
2015-09-13 22:19:04 -0400

    build it on slack

--- settings.py
+++ settings.py
@@ -3,8 +3,4 @@ import os
 HOST = "0.0.0.0"
 PORT = 8080
 
-class Config(object):
-    SECRET_KEY = os.urandom(56)
-
-class DevelopmentConfig(Config):
-    DEBUG = True
+SLACK_TOKEN = os.environ["TOKEN"]
